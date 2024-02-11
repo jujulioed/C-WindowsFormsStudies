1e0a6cee0da55e3ea3b82eb94d22a32e2353ed77
juujulio <julioeduardon9@hotmail.com>
2024-02-11 15:22:57 -0300

    Created a form to verify CPF
    Signed-off-by: juujulio <julioeduardon9@hotmail.com>

---
+++ CursoWindowsForms/Frm_ValidaCPF2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaCPF2 : Form
+    {
+        public Frm_ValidaCPF2()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+            Msk_CPF.Focus();
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+
+            string vConteudo;
+            vConteudo = Msk_CPF.Text;
+            vConteudo = vConteudo.Replace(".", "").Replace("-", "");
+            vConteudo = vConteudo.Trim();
+
+            if (vConteudo == "")
+            {
+                MessageBox.Show("Você deve digitar um CPF", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (vConteudo.Length != 11)
+                {
+                    MessageBox.Show("CPF deve ter 11 digitos", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    if (MessageBox.Show("Você deseja realmente validar o CPF?", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        Boolean validaCPF = false;
+                        validaCPF = Cls_Uteis.Valida(Msk_CPF.Text);
+                        if (validaCPF == true)
+                        {
+                            MessageBox.Show("CPF VÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("CPF INVÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }           
+        }
+
+        private void Msk_CPF_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
